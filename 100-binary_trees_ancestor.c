2dadbe2d7c525f6dfd8b82f47bac6be4dacd31db
Rivashjerrier <rivijerrier@gmail.com>
2023-03-08 14:48:53 +0000

    function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -40,3 +40,18 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	ancestor = (first && second) ? (binary_tree_t *)first : NULL;
 	return (ancestor);
 }
+
+/**
+ * binary_tree_depth - Function gives the depth of @tree node.
+ * @tree: Pointer to the node to measure the depth
+ * Description: Recursively tunnel upwards to get to the root node and sum all
+ * return values of returned stacks.
+ *
+ * Return: Depth of certain node, 0 otherwise.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL || !tree->parent)
+		return (0);
+	return (binary_tree_depth(tree->parent) + 1);
+}
